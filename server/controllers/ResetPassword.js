5be467bafbcffa09af7b59f906fd7d0fed5b2aa0
Anuj-Bhaladhare <125978464+Anuj-Bhaladhare@users.noreply.github.com>
2023-08-26 15:12:19 +0530

    creating resate handaler

--- server/controllers/ResetPassword.js
+++ server/controllers/ResetPassword.js
@@ -26,19 +26,82 @@ exports.resetPasswordToken = async(req, res) => {
                                             token: token,
                                             resetPasswordExpires: Date.now() + 5*60*1000,
                                         },
-                                        {new: true};
+                                        {new: true},
         )
+        // create url
+        const url = `http://localhost:3000/update-password/${token}`;
+        //send mail containing the url
+        await mailSender(
+                           email,
+                           "Password Reaste Link",
+                           `Password Reset Link : ${url}`  
+                        )
+        //return response
+        return res.json({
+            success:true,
+            message:'Email sent successfully, please check email and change pwd',
+        });
     }
     catch(error){
-
+        console.log(error);
+        return res.status(500).json({
+            success:false,
+            message:'Something went wrong while sending reset pwd mail'
+        })
     }
 }
 
 exports.resetPassword = async(req, res) => {
     try{
+        // fetch data
+        const {password, confirmPassword, token} = req.body;
 
+        //validation
+        if(password !== confirmPassword ) {
+            return res.status(401).json({
+                success: false,
+                message: "Password is not match",
+            })
+        }
+
+        // get user details feom db using token
+        const userDetails = await User.findOne({token: token});
+
+        //if no entry in db
+        if(!userDetails){
+            return res.status.json({
+                success: false,
+                message: "Token is invalid",
+            })
+        }
+
+        // token time check
+        if(userDetails.resetPasswordExpires < Date.now()){
+            return res.json({
+                success:false,
+                message:'Token is expired, please regenerate your token',
+            })
+        }
+
+        // hassing password
+        const hassedPassword = await bcrypt.hash(password, 10);
+
+        // password update
+        await User.findOneAndUpdate(
+            {token: token},
+            {password: hassedPassword},
+            {new: true},
+        )
+        //return response
+        return res.status(200).json({
+            success:true,
+            message:'Password reset successful',
+        });
     }
     catch(error){
-        
+        return res.status(500).json({
+            success:false,
+            message:'Something went wrong while sending reset pwd mail'
+        }) 
     }
 }
