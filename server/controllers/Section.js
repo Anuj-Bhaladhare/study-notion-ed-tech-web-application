ff1af38047b55c38a9572f87d1395642857b3469
Anuj-Bhaladhare <125978464+Anuj-Bhaladhare@users.noreply.github.com>
2023-08-28 13:59:55 +0530

    update

--- server/controllers/Section.js
+++ server/controllers/Section.js
@@ -4,10 +4,36 @@ const Course = require("../modals/Course");
 exports.createSection = async(req, res) => {
     try{
         //  Fetch data
-        // data velidation
-        // create section
-        // user validation
+        const {sectionName, courseId} = req.body;
 
+        // data velidation
+        if(!sectionName || !courseId){
+            return res.status(400).json({
+                success: false,
+                message: "Missing Properties, all field is required..!"
+            })
+        }
+
+        // create section
+        const newSection = await Section.create({sectionName});
+
+        //update course with section ObjectID
+        const updatedCourseDetails = await Course.findByIdAndUpdate(
+                                       courseId,
+                                       {
+                                            $push:{
+                                                courseContent: newSection._id,
+                                            }
+                                       } ,
+                                       {new: true},
+                                    );
+        //HW: use populate to replace sections/sub-sections both in the updatedCourseDetails
+
+        //return response
+        return res.status(200).json({
+            success: true,
+            message: "Section created successfully",
+        })
     }
     catch(error){
         return res.status(500).json({
@@ -22,7 +48,24 @@ exports.createSection = async(req, res) => {
 
 exports.updateSection = async(req, res) => {
     try{
+        // Fetch data
+        const {sectionName, sectionId} = req.body;
 
+        // data validation
+        if(!sectionName || !sectionId){
+            return res.status.json({
+                success: false,
+                message: "Missing Properties",
+            })
+        }
+        // update data
+        const section = await Section.findByIdAndUpdate(sectionId, {sectionName}, {new: true});
+
+        //return response
+        return res.status(200).json({
+            success: true,
+            message: "Section created successfully",
+        })
     }
     catch(error){
         return res.status(500).json({
@@ -38,6 +81,19 @@ exports.updateSection = async(req, res) => {
 exports.deleteSection = async(req, res) => {
     try{
 
+        //get ID - assuming that we are sending ID in params
+        const {sectionId} = req.params
+
+        //use findByIdandDelete
+        await Section.findByIdAndDelete(sectionId);
+        //TODO[Testing]: do we need to delete the entry from the course schema ??
+        
+
+        //return response
+        return res.status(200).json({
+            success: true,
+            message: "Section created successfully",
+        })
     }
     catch(error){
         return res.status(500).json({
