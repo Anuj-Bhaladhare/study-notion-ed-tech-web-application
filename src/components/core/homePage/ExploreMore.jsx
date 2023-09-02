9d90a13f40d354f052315bce77c44c6f52d41763
Anuj-Bhaladhare <125978464+Anuj-Bhaladhare@users.noreply.github.com>
2023-09-02 10:26:58 +0530

    update

--- src/components/core/homePage/ExploreMore.jsx
+++ src/components/core/homePage/ExploreMore.jsx
@@ -1,9 +1,46 @@
 import React from "react";
+import { useState } from "react";
+import HighlightText from "./HighlightText";
+import { HomePageExplore } from "../../../data/homepage-explore";
+
+const tabsName = [
+    "Free",
+    "New to coding",
+    "Most popular",
+    "Skill paths",
+    "Career paths",
+];
 
 const ExploreMore = () => {
+
+    const [currentTab, setCurrentTab] = useState(tabsName[0]);
+    
     return(
         <div>
-            ExploreMore
+            <div>
+                Unlock the
+                <HighlightText text={"power of Code"}/>
+            </div>
+
+            <p>
+                 Learn to build anything you can imagine   
+            </p>
+
+            <div className="">
+                {
+                    tabsName.map( (element, index) => {
+                        return(
+                            <div 
+                            key={index}
+                            className={`${currentTab === element ? "bg-richblack-900 text-richblack-5 font-medium" : "text-richblack-200"}
+                                       flex flex-row items-centerm gap-2 text-[16px] rounded-full transition-all duration-200 cursor-pointer hover:bg-richblack-900 hover:text-richblack-5 px-7 py-2`}
+                            > {element} </div>    
+                        )
+                    })
+                }
+            </div>
+
+            <div className="lg:h-[150px]"></div>
         </div>
     )
 }
--- src/components/core/homePage/InstructorSection.jsx
+++ src/components/core/homePage/InstructorSection.jsx
@@ -1,9 +1,37 @@
 import React from "react";
+import HighlightText from "./HighlightText";
+import Instructor from "../../../assets/Images/Instructor.png";
+import { FaArrowRight } from "react-icons/fa"
+import CTAButton from "./Button";
 
 const InstructorSection = () => {
     return(
-        <div>
-            InstructorSection
+        <div className="w-11/12 flex flex-row gap-9">
+            <div>
+                <img 
+                src={Instructor} 
+                alt="Instructor" 
+                className=""
+                />
+            </div>
+
+            <div className="flex flex-col text-start">
+                <div className="text-4xl flex flex-col">
+                    Become an 
+                    <HighlightText text={"Instructor"}/>
+                </div>
+                <p>
+                    Instructors from around the world teach millions of students on StudyNotion. We provide the tools and skills to teach what you love.
+                </p>
+                <div className="w-fit">
+                    <CTAButton active={true} linkto={"/singup"}>
+                        <div className="flex flex-row items-center gap-2">
+                            Start Teaching Today
+                            <FaArrowRight />
+                        </div>
+                    </CTAButton>
+                </div>
+            </div>
         </div>
     )
 }
