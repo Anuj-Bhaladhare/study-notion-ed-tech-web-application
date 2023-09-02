6bf8b449e0c54d2cf3334c013db3d961fb485751
Anuj-Bhaladhare <125978464+Anuj-Bhaladhare@users.noreply.github.com>
2023-09-02 19:34:51 +0530

    update

--- src/components/core/Auth/LoginForm.jsx
+++ src/components/core/Auth/LoginForm.jsx
@@ -1,9 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
+import { AiOutlineEyeInvisible, AiOutlineEye} from "react-icons/ai";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+
  const LoginForm = () => {
+    
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const [formData, setFormData] = useState({
+        email:"",
+        password:"",
+    })
+
+    const [showPassword, setShowPassword] = useState(false);
+
+    const { email, password } = formData;
+
+    const handleOnChange = (e) => {
+        setFormData((prevData) => ({
+          ...prevData,
+          [e.target.name]: e.target.value,
+        }))
+      }
+    
+      const handleOnSubmit = (e) => {
+        e.preventDefault()
+        dispatch(login(email, password, navigate))
+      }
+
     return(
-        <div>
-            LoginForm
-        </div>
+        <form onSubmit={handleOnSubmit} className="">
+
+            <label>
+                <p>Email Address<sup>*</sup></p>
+                <input 
+                  required
+                  type="text"
+                  name="email"
+                  onChange={handleOnChange}
+                  value={email}
+                  placeholder="Enter Email Eddress"
+                  style={{
+                    boxShadow: "inset 0px -1px 0px rgba(255, 255, 255, 0.18)",
+                  }}
+                  className=""
+                />
+            </label>
+
+            <label>
+               <p>Password<sup>*</sup></p> 
+               <input 
+                 required
+                 type="password"
+                 placeholder="Enter Your Password"
+                 value={password}
+                 onChange={handalOnSubmit}
+                 style={{
+                    boxShadow: "inset 0px -1px 0px rgba(255, 255, 255, 0.18)",
+                 }}
+                />
+                <span onClick={() => setShowPassword((prev) => !prev)} className="absolute right-3 top-[38px] z-[10] cursor-pointer"> 
+                   {
+                    showPassword ? 
+                    (<AiOutlineEyeInvisible fontSize={24} fill="#AFB2BF"/>) :
+                    (<AiOutlineEye fontSize={24} fill="#AFB2BF"/>)
+                   }
+                </span>
+                <Link to="/forgot-password">
+                  <p className="mt-1 ml-auto max-w-max text-xs text-blue-100">
+                    Forgot Password
+                  </p>
+                </Link>
+            </label>
+
+            <button type="submit" className="mt-6 rounded-[8px] bg-yellow-50 py-[8px] px-[12px] font-medium text-richblack-900">
+                Sign In
+            </button>
+        </form>
     )
  }
 
--- src/components/core/Auth/Template.jsx
+++ src/components/core/Auth/Template.jsx
@@ -2,70 +2,67 @@ import React from "react";
 import LoginForm from "./LoginForm";
 import SingupForm from "./SingupForm";
 import frameImage from "../../../assets/Images/frame.png";
+import { useSelector } from "react-redux";
 
 
  const Template = (title, description1, description2, image, formType) => {
+    const {loading} = useSelector((state) => state.auth)
     return(
-        <div>
-            {/* section 1 */}
-            <div>
-                <h1>
-                    {title}
-                </h1>
-                <div>
-                    <p>{description1}</p>
-                    <p>{description2}</p>
-                </div>
-                <div>
-                    <button>
-                        Student
-                    </button>
-                    <button>
-                        Instructor
-                    </button>
-                </div>
-                <div>
-                    {
-                        formType === "signup" ? (<SingupForm />) : (<LoginForm />)
-                    }
-                </div>
-                <div>
-                    {
-                        formType === "signup" ? 
-                        (
+       <div className="grid min-h-[calc(100vh-3.5rem)] place-items-center">
+         {
+            loading ?
+            (<div className="Spinner"></div>) : 
+            (
+                <div className="mx-auto flex w-11/12 max-w-maxContent flex-col-reverse justify-between gap-y-12 py-12 md:flex-row md:gap-y-0 md:gap-x-12">
+                    {/* section 1 */}
+                    <div className="mx-auto w-11/12 max-w-[450px] md:mx-0">
+                        <h1 className="text-[1.875rem] font-semibold leading-[2.375rem] text-richblack-5">
+                            {title}
+                        </h1>
+                        <div className="mt-4 text-[1.125rem] leading-[1.625rem]">
+                            <p className="text-richblack-100">{description1}</p>
+                            <p className="font-edu-sa font-bold italic text-blue-100">{description2}</p>
+                        </div>
+
+                        {/* <div>
                             <button>
-                                Create Accounr
+                                Student
                             </button>
-                        ) : 
-                        (
                             <button>
-                                Sing In
+                                Instructor
                             </button>
-                        )
-                    }
+                        </div> */}
+                        
+                        <div>
+                            {
+                                formType === "signup" ? (<SingupForm />) : (<LoginForm />)
+                            }
+                        </div>
+        
+                    </div>
+    
+                    {/* Section */}
+                    <div className="relative mx-auto w-11/12 max-w-[450px] md:mx-0">      
+                        <img 
+                            src={frameImage} 
+                            alt="Pattern"
+                            width={558}
+                            height={504}
+                            loading="lazy" 
+                        />
+        
+                        <img 
+                            src={image} 
+                            width={558}
+                            height={504}
+                            loading="lazy"
+                            className="absolute -top-4 right-4 z-10"
+                        />                  
+                    </div>
                 </div>
-
-            </div>
-
-            {/* Section */}
-            <div>
-
-              <div>
-                <img 
-                    src={frameImage} 
-                    alt="frameImage"
-                    className="" />
-              </div>
-
-              <div>
-                <img 
-                    src={image} 
-                    alt="image"
-                    className="" />
-              </div>
-              
-            </div>
-        </div>
+            )
+         }
+       </div>
     )
  }
 
