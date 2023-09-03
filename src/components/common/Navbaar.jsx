7e6a83ca89fd3ae7e5474e292b1ca645fb0c1796
Anuj-Bhaladhare <125978464+Anuj-Bhaladhare@users.noreply.github.com>
2023-09-03 11:20:30 +0530

    update navbaar

--- src/components/common/Navbaar.jsx
+++ src/components/common/Navbaar.jsx
@@ -3,6 +3,16 @@ import Logo from "../../assets/Logo/Logo-Full-Light.png";
 import { Link, useLocation } from "react-router-dom"; // Changed `matchPath` to `useLocation`
 import { NavbarLinks } from "../../data/navbar-links";
 
+const subLinks = [
+    {
+        title: "python",
+        link:"/catalog/python"
+    },
+    {
+        title: "web dev",
+        link:"/catalog/web-development"
+    },
+];
 
 const Navbar = () => {
     const location = useLocation(); // Added to get the current location
@@ -24,7 +34,26 @@ const Navbar = () => {
                             return (
                                 <li key={index}>
                                     {link.title === "Catalog" ? (
-                                        <div>{/* You can add content here if needed */}</div>
+                                        <div className="relative flex items-center gap-2 group">
+                                            <p>{link.title}</p>
+                                            <IoIosArrowDropdownCircle />
+
+                                            <div className="invisible absolute left-[50%] translate-x-[-50%] translate-y-[80%] top-[50%] flex flex-col rounded-md bg-richblack-5 p-4 text-richblue-900 opacity-0 transition-all duration-200 group-hover:visible group-hover:opacity-100 lg:w-[300px]">
+                                                <div className="absolute left-[50%] top-0 translate-x-[80%] translate-y-[45%] h-6 w-6 rotate-45 rounded bg-richblack-5">
+                                                    {
+                                                        subLinks.map( (sublink, index) => {
+                                                            return(
+                                                                <div key={index}>
+                                                                    <Link to={`${sublink.path}`}>
+                                                                         <p>{sublink.title}</p>
+                                                                    </Link>
+                                                                </div>
+                                                            )
+                                                        })
+                                                    }
+                                                </div>
+                                            </div>
+                                        </div>
                                     ) : (
                                         <Link to={link.path}>
                                             <p
@@ -44,20 +73,11 @@ const Navbar = () => {
                     </ul>
                 </nav>
 
+                {/* Login / SingUp / dashboard */}
                 <div>
-                    <ul>
-                        <li>
-                            <Link to="/login" >
-                               login
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="/singup" >
-                               singup
-                            </Link>                           
-                        </li>
-                    </ul>
+
                 </div>
+
             </div>
         </div>
     );
