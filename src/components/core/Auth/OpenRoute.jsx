558aed0932526a4b3e0a367fb26a739cd03a2e06
Anuj-Bhaladhare <125978464+Anuj-Bhaladhare@users.noreply.github.com>
2023-09-04 10:09:13 +0530

    update

--- src/components/core/Auth/OpenRoute.jsx
+++ src/components/core/Auth/OpenRoute.jsx
@@ -1,10 +1,18 @@
+// This will prevent authenticated users from accessing this route
+
 import React from "react";
- const OpenRoute = () => {
-    return(
-        <div>
-            OpenRoute
-        </div>
-    )
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+
+ const OpenRoute = ({ children }) => {
+
+    const { Token } = useSelector((state) => state.auth);
+
+    if(Token === null){
+       return children
+    } else{
+        return <Navigate to="/dashboard/my-profile" />
+    }    
  }
 
  export default OpenRoute;
