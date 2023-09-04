46583343f33ceba7ec0e47b6fbc25c28fdc53088
Anuj-Bhaladhare <125978464+Anuj-Bhaladhare@users.noreply.github.com>
2023-09-04 14:25:49 +0530

    update backend

--- server/models/Section.js
+++ server/models/Section.js
@@ -1,19 +1,18 @@
 const mongoose = require("mongoose");
 
-const RatingAndReview = new mongoose.Schema({
+// Define the Section schema
+const sectionSchema = new mongoose.Schema({
+	sectionName: {
+		type: String,
+	},
+	subSection: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			required: true,
+			ref: "SubSection",
+		},
+	],
+});
 
-    sectionName:{
-        type: String,
-    },
-
-    subSection: [
-        {
-            type:mongoose.Schema.Types.ObjectId,
-            required: true, 
-            ref:"SubSection",   
-        }
-    ],
-
-})
-
-module.exports = mongoose.model("Section", RatingAndReview);
+// Export the Section model
+module.exports = mongoose.model("Section", sectionSchema);
