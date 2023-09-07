21dc2b29e8eeac2e8ed6a0ed0d0d8d6486797399
Anuj-Bhaladhare <125978464+Anuj-Bhaladhare@users.noreply.github.com>
2023-09-07 16:50:54 +0530

    update

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,29 @@
-import React from "react"
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Sidebar from "../components/core/Dashboard/Sidebar";
+import { useSelector } from "react-redux";
  
 const Dashboard = () => {
+
+    const { loading: profileLoading } = useSelector((state) => state.profile);
+    const { loading: authLoading } = useSelector((state) => state.auth);
+
+    if(profileLoading || authLoading){
+        return(
+            <div className="grid min-h[calc(100vh-3.5rem)] place-content-center">
+                <div className="custom-loader"></div>
+            </div>
+        )
+    }
+
     return(
-        <div>
-           Dashboard 
+        <div className="relative flex min-h-[calc(100vh-3.5rem)]">
+            <Sidebar />
+           <div className="h-[calc(100vh-3.5rem)] flex-1 overflow-auto">
+              <div className="mx-auto w-11/12 max-w-[1000px] py-10">
+                <Outlet />
+              </div>
+           </div> 
         </div>
     )
 }
