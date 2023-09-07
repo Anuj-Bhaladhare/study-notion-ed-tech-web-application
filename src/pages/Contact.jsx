2959d31f43673872453f7a885c7cb4b24308e7ad
Anuj-Bhaladhare <125978464+Anuj-Bhaladhare@users.noreply.github.com>
2023-09-07 15:53:09 +0530

    update

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,36 @@
 import React from "react"
+import Footer from "../components/common/Footer";
+import ContactDetails from "../components/ContactPage/ContactDetails";
+import ContactForm from "../components/ContactPage/ContactForm";
  
 const Contact = () => {
     return(
         <div>
-           Contact 
+           {/* Section 1 */}
+           <div>
+                {/* Contact Details */}
+                <div>
+                    <ContactDetails />
+                </div>
+                {/* Contact Form */}
+                <div>
+                    <ContactForm />
+                </div> 
+           </div>
+
+           {/* Section 2 */}
+           <div>
+                {/* Reviws from Other Learner */}
+                <h1>Review From other learning</h1>
+                <div>
+                    {/* <ReviewSlider /> */}
+                </div>
+           </div>
+
+           {/* Section 3 */}
+           <div>
+             <Footer />
+           </div>
         </div>
     )
 }
