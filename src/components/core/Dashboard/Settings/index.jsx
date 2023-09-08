a64b28589c8537decb930d09e8a680bcfef0f3e5
Anuj-Bhaladhare <125978464+Anuj-Bhaladhare@users.noreply.github.com>
2023-09-08 14:38:17 +0530

    update

--- src/components/core/Dashboard/Settings/index.jsx
+++ src/components/core/Dashboard/Settings/index.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react";
+import ChangeProfilePicture from "./ChangeProfilePicture";
+import EditProfile from "./EditProfile";
+import UpdatePassword from "./UpdatePassword";
+import DeleteAccount from "./DeleteAccount";
 
 const index = () => {
-    return (
-        <div>
-                
-        </div>
-    );
-}
+  return (
+    <div>
+      <h1 className="mb-14 text-3xl font-medium text-richblack-5">
+        Edit Profile
+      </h1>
+      {/* Change Profile Picture */}
+      <ChangeProfilePicture />
+      {/* Profile */}
+      <EditProfile />
+      {/* Password */}
+      <UpdatePassword />
+      {/* Delete Account */}
+      <DeleteAccount />
+    </div>
+  );
+};
 
 export default index;
