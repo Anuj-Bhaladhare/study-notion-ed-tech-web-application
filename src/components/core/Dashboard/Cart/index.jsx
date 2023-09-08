6b235309ed2c59bf25db1b466d18efda56f164ae
Anuj-Bhaladhare <125978464+Anuj-Bhaladhare@users.noreply.github.com>
2023-09-08 13:39:12 +0530

    update

--- src/components/core/Dashboard/Cart/index.jsx
+++ src/components/core/Dashboard/Cart/index.jsx
@@ -1,13 +1,27 @@
-import React, { Component } from 'react';
+import React from "react";
+import { useSelector } from "react-redux";
+import RenderCartCourses from "./RenderCartCourses";
+import RenderTotalAmount from "./RenderTotalAmount";
 
-class index extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
+
+const Cart = () => {
+
+    const { total, totalItems } = useSelector((state)=>state.auth);
+
+    return(
+        <div className="text-white">
+            <h1>Your Cart</h1>
+            <p>{totalItems} Courses in Cart</p>
+            {
+                total > 0 ?
+                (<div>
+                    <RenderCartCourses />
+                    <RenderTotalAmount />
+                </div>) :
+                (<p>Your Cart is Empty</p>)
+            }
+        </div>
+    )
 }
 
-export default index;
+export default Cart;
