9bdd81e7c1a2058d267009c7d5a780e6bd6a06d2
Anuj-Bhaladhare <125978464+Anuj-Bhaladhare@users.noreply.github.com>
2023-09-08 10:28:52 +0530

    update iconbtn

--- src/components/core/Dashboard/Sidebar.jsx
+++ src/components/core/Dashboard/Sidebar.jsx
@@ -1,10 +1,35 @@
 import React from "react"
+import { sidebarLinks } from "../../../data/dashboard-links";
+import { logout } from "../../../services/operations/authAPI";
+import { useSelector } from "react-redux";
+import SidebarLink from "./SidebarLink";
  
 const Sidebar = () => {
+
+    const { user, loading: profileLoading } = useSelector(
+        (state) => state.profile
+    );
     return(
         <div>
-           Sidebar 
+            <div>
+                {
+
+                }
+            </div>
+            <div />
+            <div>
+                <SidebarLink />
+                <button onClick={() => {
+                    text1: "Are you sure?",
+                }}>
+                    <div>
+                     <VscSignOut />
+                     <span>Logout</span>
+                    </div>
+                </button>
+            </div>
         </div>
+        {}
     )
 }
 
