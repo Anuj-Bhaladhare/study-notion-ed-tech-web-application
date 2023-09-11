f672c5bd3fe16684fc50fafe360c88fcc13ff5eb
Anuj-Bhaladhare <125978464+Anuj-Bhaladhare@users.noreply.github.com>
2023-09-11 17:08:18 +0530

    update backend

--- src/components/core/Dashboard/AddCourse/CourseBuilder/CourseBuilderForm.jsx
+++ src/components/core/Dashboard/AddCourse/CourseBuilder/CourseBuilderForm.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import { MdAddCircleOutline } from "react-icons/md"
 
 const CourseBuilderForm = () => {
+
+  const { register, handleSubmit, setValue, formState: { errors } } = useForm();
   return (
     <div>
       <p>Course Builder</p>
@@ -8,7 +12,34 @@ const CourseBuilderForm = () => {
         <div>
           <label>Section Name<sup>*</sup></label>
           <input 
-          />
+            id='sectionName'
+            placeholder='add section name'
+            {...register("sectionName", {required: true})}
+            className='w-full'
+            />
+            {errors.sectionName && (
+               <span>Section Name is required</span>
+            )}
+        </div>
+        <div>
+          <IconBtn
+            type="submit"
+            text={editSectionName ? "Edit section name" : "create section"}
+            outLine={true}
+          >
+             <MdAddCircleOutline className='text-yellow-50' size={20}/>
+          </IconBtn>
+          {
+            editSectionName && (
+              <button
+                type='button'
+                onClick={cancleEdit}
+                className='text-ms text-richblack-300 underline ml-10'
+              >
+                Cancle Edit
+              </button>
+            )
+          }
         </div>
       </form>
     </div>
