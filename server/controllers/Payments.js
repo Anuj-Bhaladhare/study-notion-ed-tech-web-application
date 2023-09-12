82d7e704d0bc4e4c9059ae96dd687c4cc59d99af
Anuj-Bhaladhare <125978464+Anuj-Bhaladhare@users.noreply.github.com>
2023-09-12 16:02:23 +0530

    create verify Payment controller

--- server/controllers/Payments.js
+++ server/controllers/Payments.js
@@ -67,12 +67,34 @@ exports.capturePayment = async(res, req) => {
 
 //verify the payment
 exports.verifyPayment = async(res, req) => {
-    try{
 
-    }
-    catch(error){
+    const razorpay_order_id = req.body?.razorpay_order_id;
+    const razorpay_payment_id = req.body?.razorpay_payment_id;
+    const razorpay_signature = req.body?.razorpay_signature;
+    const courses = req.body?.courses;
+    const userId = req.user.id;
 
+    if(!razorpay_order_id ||
+       !razorpay_payment_id ||
+       !razorpay_signature ||
+       !courses ||
+       !userId) {
+            return res.status(200).json({success: false, message: "Payment Failed"})
     }
+
+    let body = razorpay_order_id + "|" + razorpay_payment_id;
+    const expectedSignature = crypto
+    .createHmac("sha256", process.env.RAZORPAY_SECRET)
+    .update(body.toString())
+    .digest("hex");
+
+    if(expectedSignature === razorpay_signature){
+        //enroll karwao student ko
+        await enrollStudents(courses, userId, res);
+        //return res
+        return res.status(200).json({success:true, message:"Payment Verified"});
+    }
+    return res.status(200).json({success:"false", message:"Payment Failed"});
 }
 
 
