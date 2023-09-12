64a74a6a65b6c67a5f0c8fbb81f8efb073c7f8e4
Anuj-Bhaladhare <125978464+Anuj-Bhaladhare@users.noreply.github.com>
2023-09-12 13:59:02 +0530

    update

--- src/components/core/homePage/CodeBlocks.jsx
+++ src/components/core/homePage/CodeBlocks.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import CTAButton from "../HomePage/Button"
-import HighlightText from './HighlightText'
+import CTAButton from "../homePage/Button"
+// import HighlightText from './highlightText'
 import {FaArrowRight} from "react-icons/fa"
 import { TypeAnimation } from 'react-type-animation'
 
