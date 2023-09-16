bbba26dd865a3b267aa2317597ff09678bbaaefd
Anuj-Bhaladhare <125978464+Anuj-Bhaladhare@users.noreply.github.com>
2023-09-16 09:43:11 +0530

    update

---
+++ src/services/operations/studentFeaturesAPI.js
@@ -0,0 +1,23 @@
+import { toast } from "react-router-dom";
+import { studentEndpoints } from "../apis";
+import { apiConnector } from "../apiconnector";
+import rzLogo from "../../assets/Logo/rzp_logo.png";
+import { setPaymentLoading } from "../../slices/courseSlice";
+import { resetCart } from "../../slices/cartSlice";
+
+const {COURSE_PAYMENT_API, COURSE_VERIFY_API, SEND_PAYMENT_SUCCESS_EMAIL_API} = studentEndpoints;
+
+const loadScript = (src) => {
+    return new Promise((resolve) => {
+        const script = document.createElement("script");
+        script.src = src;
+
+        script.onload = () => {
+            resolve(true);
+        }
+        script.onerror= () =>{
+            resolve(false);
+        }
+        document.body.appendChild(script);
+    })
+}
